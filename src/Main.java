ed961ed5a651fdde78158c5e9c614d0604838577
UserHub <bender_rodriguez_93@mail.ru>
2022-03-21 17:09:13 +0300

    com1 from NP

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello World! ");
         System.out.println("Hello World1! ");
         System.out.println("NPRep! ");
+        System.out.println("NPRep1! ");
 
     }
 }
