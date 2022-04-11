71709f7094b774d74e7665de7d4d17e8039b3281
IBA_Hub <bender_rodriguez_93@mail.ru>
2022-04-11 17:25:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello World1! ");
         System.out.println("NPRep! ");
         System.out.println("NPRep1! ");
+        System.out.println("User11/04 ");
 
     }
 }
