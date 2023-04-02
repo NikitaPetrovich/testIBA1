847f6d64a1328d27fb2ddec7d48f0f7e0d955bc4
UserHub <bender_rodriguez_93@mail.ru>
2023-04-02 15:17:56 +0300

    commit from 1 account

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("User11/04 ");
 System.out.println("User/11/04 ");
         System.out.println("User/11/04 ");
+System.out.println("User/11/04 ");
     }
 }
